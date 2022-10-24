a30c737a415b302315ab06b236d1138f182a139e
eliomaure2022 <elio_maure@yahoo.com.ar>
2022-10-24 17:39:18 -0300

    add color button

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import Button from './Components/Button'
 
 
 function App() {
-  const [index, setIndex]=useState(Math.floor(Math.random() * quotes.length))
-  const changeText=()=>{
-   setIndex( Math.floor(Math.random() * quotes.length));
+  const [index, setIndex] = useState(Math.floor(Math.random() * quotes.length))
+  const changeText = () => {
+    setIndex(Math.floor(Math.random() * quotes.length));
   }
 
   const colors = [
@@ -23,18 +23,21 @@ function App() {
     '#5A6DEC',
     '#5AB9EC',
   ]
-  const changeColor= Math.floor(Math.random() * colors.length);
-  document.body.style=`background: ${colors[changeColor]} `
-  
-  
+  const changeColor = Math.floor(Math.random() * colors.length);
+  document.body.style = `background: ${colors[changeColor]} `
+
+
   return (
-    <div className="App" style={{color:colors[changeColor]}}>
-      <QuoteBox index ={index} changeText={changeText} />
+    <div className="App" style={{ color: colors[changeColor] }}>
+      <QuoteBox index={index} changeText={changeText} />
+      <Button changeText={changeText} colors={colors} changeColor={changeColor} />
     </div>
   )
 }
 
 
+
+
 export default App
 
-      
+
--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Button = ({changeText,}) => {
+const Button = ({changeText, colors, changeColor}) => {
   return (
-    <div className='but' >
-      <button onClick={changeText} style={{}}>{<i className="fa-solid fa-shuffle"></i>}</button>
+    <div className='but'  >
+      <button onClick={changeText} style={{background:colors[changeColor]}} >{<i className="fa-solid fa-shuffle"></i>}</button>
     </div>
   );
 };
